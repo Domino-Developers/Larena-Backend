4a3ffb764c7c3dc4fb035744aef97a8604adcc4c
tichnas <sanchit.arora.2002@gmail.com>
2020-12-21 01:28:52 +0530

    Query Products Fixes #17

--- backend/ecommerce/schema.py
+++ backend/ecommerce/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from graphql_jwt.decorators import login_required
+from django.db.models import Q
 
 from .models import *
 
@@ -11,7 +12,12 @@ class CartType(DjangoObjectType):
         exclude = ("user",)
 
 
-class PProductType(DjangoObjectType):
+class ReviewType(DjangoObjectType):
+    class Meta:
+        model = Review
+
+
+class ProductType(DjangoObjectType):
     class Meta:
         model = Product
 
@@ -30,6 +36,37 @@ class UserType(DjangoObjectType):
 class Query(graphene.ObjectType):
     me = graphene.Field(UserType)
 
+    products = graphene.List(
+        ProductType,
+        first=graphene.Int(),
+        skip=graphene.Int(),
+        search=graphene.String(),
+        kind=graphene.String(),
+    )
+    product = graphene.Field(ProductType, id=graphene.String())
+
     @login_required
     def resolve_me(self, info):
         return info.context.user
+
+    def resolve_product(self, info, id, **kwargs):
+        return Product.objects.get(pk=id)
+
+    def resolve_products(
+        self, info, first=None, skip=None, search=None, kind=None, **kwargs
+    ):
+        qs = Product.objects.all()
+
+        if search:
+            qs = qs.filter(Q(name__icontains=search))
+
+        if kind:
+            qs = qs.filter(Q(kind=kind))
+
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
